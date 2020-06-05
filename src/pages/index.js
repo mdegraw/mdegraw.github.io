183c0e3ea3d8c730ff7b12f4a0b62d7474665c08
mdegraw <michael.degraw@protonmail.com>
2020-06-05 17:47:38 -0600

    Update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="mdegraw.github.io" />
-    <h1>Projects</h1>
-    <p>Stuff I'm tinkering around with.</p>
-    <a href="http://mdegraw.github.io/rxjs-drum-machine">RxJS Drum Machine</a> <br />
+    <SEO title='michael degraw projects' />
+    <h2>Stuff I'm tinkering around with:</h2>
+    <a style={{ color: '#663399'}} href='http://mdegraw.github.io/rxjs-drum-machine'>RxJS Drum Machine</a>&nbsp;<a style={{ color: '#663399'}} href='http://github.com/mdegraw/rxjs-drum-machine'>(github)</a>
+    <p>A simple drum machine using RxJS as an event pipeline/clock</p>
+    <br />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
